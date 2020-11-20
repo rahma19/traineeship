6d41eb6b820225f882c7556fe788a76ddeb042bf
rahma19 <56127492+rahma19@users.noreply.github.com>
2020-11-20 16:37:29 +0100

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CreerCompteComponent } from './authentification/creer-compte/creer-compte.component';
+import { ErreurComponent } from './authentification/erreur/erreur.component';
+import { LoginComponent } from './authentification/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accueil', component:AccueilComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signUp', component:CreerCompteComponent},
+{path:'', redirectTo:'accueil', pathMatch:'full'},
+{path:'**',component:ErreurComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreerCompteComponent } from './authentification/creer-compte/creer-compte.component';
+import { LoginComponent } from './authentification/login/login.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ErreurComponent } from './authentification/erreur/erreur.component';
+import { NavbarComponent } from './accueil/navbar/navbar.component';
+import { FooterComponent } from './accueil/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreerCompteComponent,
+    LoginComponent,
+    AccueilComponent,
+    ErreurComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
